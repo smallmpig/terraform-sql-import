2fffe96cd63366801a8a918925d7d1ac8918ec89
Raven Liu <raven.liu@softbi.com>
2023-11-03 16:10:13 +0800

    test import

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ import {
   to = google_sql_database_instance.postgre_sql
 }
 
-# resource google_sql_database_instance postgre_sql {
-#   database_version = "POSTGRES_15"
-#   project = "peppy-ward-398202"
-#   name = "raven-test"
-#   deletion_protection = true
-#   clone {
-#     source_instance_name = "raven-test"
-#   }
+data google_sql_database_instance postgre_sql {
+  database_version = "POSTGRES_15"
+  project = "peppy-ward-398202"
+  name = "raven-test"
+  deletion_protection = true
+  clone {
+    source_instance_name = "raven-test"
+  }
 
-# }
+}
 
 resource "google_sql_database" "cloud_database" {
     name = "my_db"
