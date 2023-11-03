ac1f8eefd8b25c88b8f7da15aeeae764d447d043
Raven Liu <raven.liu@softbi.com>
2023-11-03 15:54:40 +0800

    test import

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "sql_user" {
+  value = google_sql_user.sql_user.name
+}
+
+output "sql_password" {
+    value = google_sql_user.sql_user.password
+}
+
+output "databae_name" {
+    value = google_sql_database.cloud_database.name
+}
